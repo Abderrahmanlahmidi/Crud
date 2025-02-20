d9f887e2c3d63951462d7d7b1b16c5a9a01f7aed
unknown <zz240614@gmail.com>
2025-02-20 16:06:21 +0100

    Create add Controller methods

--- App/Controllers/ArticleController.php
+++ App/Controllers/ArticleController.php
@@ -1,16 +1,28 @@
 <?php
+class ArticleController
+{
 
-class ArticleController {
+    private Article $articleModel;
 
- private Article $articleController;
+    public function __construct()
+    {
+        $this->articleModel = new Article();
+    }
 
- public function __construct()
- {
-    return $this->articleController = new Article();
- }
-
- public function addArticle()
- {
-    
- }
+    public function addArticle()
+    {
+        if (isset($_POST['submit'])) {
+            $titre = $_POST['titre'];
+            $contenu = $_POST['contenu'];
+            $dateDePublication = $_POST['dateDePublication'];
+        }
+        try {
+            $article =  new Article();
+            $article->setTitre($titre);
+            $article->setContenu($contenu);
+            $article->setDateDePublication($dateDePublication);
+        } catch (PDOException $e) {
+            error_log('eror to add article' . $e->getMessage());
+        }
+    }
 }
